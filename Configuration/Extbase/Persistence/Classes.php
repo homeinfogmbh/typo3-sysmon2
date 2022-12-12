1fe81376456a3914ceadca625e2dde45b48f73cd
Richard Neumann <r.neumann@homeinfo.de>
2022-12-12 10:34:50 +0100

    Remove manual mappings

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,15 +3,7 @@ declare(strict_types = 1);
 
 return [
     \Homeinfo\SysMon2\Domain\Model\Person::class => [
-        'tableName' => 'tt_address',
+        'tableName' => 'checkresults',
         'recordType' => \Homeinfo\SysMon2\Domain\Model\CheckResults::class,
-        'properties' => [
-            'dateOfBirth' => [
-                'fieldName' => 'birthday',
-            ],
-            'thoroughfare' => [
-                'fieldName' => 'street',
-            ],
-        ],
     ],
 ];
