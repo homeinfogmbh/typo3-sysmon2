81d4696ff9f9df074480a0bea0136b04f9a9bf2d
Richard Neumann <r.neumann@homeinfo.de>
2022-12-27 10:28:16 +0100

    Update extension key

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,10 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$extensionKey = 'sysmon2';
+$extensionKey = 'SysMon2';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'sysmon2',
+    'SysMon2',
     'SysMon2',
     [
         \Homeinfo\SysMon2\Controller\DebugController::class => 'debug',
