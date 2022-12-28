2fa18206b59c932305060d02d655d84d9c1f5a82
Richard Neumann <r.neumann@homeinfo.de>
2022-12-28 12:16:05 +0100

    Write model

--- Classes/Domain/Model/CheckResults.php
+++ Classes/Domain/Model/CheckResults.php
@@ -2,122 +2,61 @@
 
 namespace Homeinfo\SysMon2\Domain\Model;
 
-use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
-
-class CheckResults extends AbstractEntity
+final class CheckResults
 {
-    /**
-     * @var int $id
-     */
-    public $id;
+    function __construct(
+        public readonly int $id,
+        public readonly DateTime $timestamp,
+        public readonly int $system,
+        public readonly bool $icmp_request,
+        public readonly string $ssh_login,
+        public readonly string $http_request,
+        public readonly string $application_state,
+        public readonly string $smart_check,
+        public readonly string $baytrail_freeze,
+        public readonly string $fsck_repair,
+        public readonly bool $application_version,
+        public readonly int $ram_total,
+        public readonly int $ram_free,
+        public readonly int $ram_available,
+        public readonly string $efi_mount_ok,
+        public readonly int $download,
+        public readonly int $upload,
+        public readonly string $root_not_ro,
+        public readonly string $sensors,
+        public readonly bool $in_sync,
+        public readonly int $recent_touch_events,
+        public readonly DateTime $offline_since,
+        public readonly DateTime $blackscreen_since
+    )
+    {
+    }
 
-    /**
-     * @var DateTime $timestamp
-     */
-    public $timestamp;
-
-    /**
-     * @var int $system
-     */
-    public $system;
-
-    /**
-     * @var bool $icmp_request 
-     */
-    public $icmp_request;
-
-    /**
-     * @var string $ssh_login  
-     */
-    public $ssh_login ;
-
-    /**
-     * @var string $http_request 
-     */
-    public $http_request;
-
-    /**
-     * @var string $application_state 
-     */
-    public $application_state;
-
-    /**
-     * @var string $smart_check 
-     */
-    public $smart_check;
-
-    /**
-     * @var string $baytrail_freeze 
-     */
-    public $baytrail_freeze;
-
-    /**
-     * @var string $fsck_repair 
-     */
-    public $fsck_repair;
-
-    /**
-     * @var bool $application_version 
-     */
-    public $application_version;
-
-    /**
-     * @var int $ram_total 
-     */
-    public $ram_total;
-
-    /**
-     * @var int $ram_free 
-     */
-    public $ram_free;
-
-    /**
-     * @var int $ram_available 
-     */
-    public $ram_available;
-
-    /**
-     * @var string $efi_mount_ok 
-     */
-    public $efi_mount_ok;
-
-    /**
-     * @var int $download 
-     */
-    public $download;
-
-    /**
-     * @var int $upload 
-     */
-    public $upload;
-
-    /**
-     * @var string $root_not_ro 
-     */
-    public $root_not_ro;
-
-    /**
-     * @var string $sensors 
-     */
-    public $sensors;
-
-    /**
-     * @var bool $in_sync 
-     */
-    public $in_sync;
-
-    /**
-     * @var int $recent_touch_events 
-     */
-    public $recent_touch_events;
-
-    /**
-     * @var DateTime $offline_since 
-     */
-    public $offline_since;
-
-    /**
-     * @var DateTime $blackscreen_since 
-     */
-    public $blackscreen_since;
+    public static function fromArray($array) {
+        return new self(
+            $array['id'],
+            new DateTime($array['timestamp']),
+            $array['system'],
+            $array['icmp_request'],
+            $array['ssh_login'],
+            $array['http_request'],
+            $array['application_state'],
+            $array['smart_check'],
+            $array['baytrail_freeze'],
+            $array['fsck_repair'],
+            $array['application_version'],
+            $array['ram_total'],
+            $array['ram_free'],
+            $array['ram_available'],
+            $array['efi_mount_ok'],
+            $array['download'],
+            $array['upload'],
+            $array['root_not_ro'],
+            $array['sensors'],
+            $array['in_sync'],
+            $array['recent_touch_events'],
+            ($array['offline_since'] === null) ? null : new DateTime($array['offline_since']),
+            ($array['blackscreen_since'] === null) ? null : new DateTime($array['blackscreen_since']),
+        );
+    }
 }
