8de99a5759a85cba86ff61fa629eecc0489f9546
Richard Neumann <r.neumann@homeinfo.de>
2022-12-28 10:48:39 +0100

    Debug query
    While at it, order imports

--- Classes/Controller/DebugController.php
+++ Classes/Controller/DebugController.php
@@ -2,10 +2,10 @@
 
 namespace Homeinfo\SysMon2\Controller;
 
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use Homeinfo\SysMon2\Domain\Repository\CheckResultsRepository;
@@ -18,7 +18,7 @@ class DebugController extends ActionController
         $repository = GeneralUtility::makeInstance(ObjectManager::class)
             ->get(CheckResultsRepository::class);
         $records = $repository->findBySystem(12);
-        DebuggerUtility::var_dump($records);
+        DebuggerUtility::var_dump($records, "Records: ");
         $this->view->assign('check_results', $records);
     }
 }
--- Classes/Domain/Repository/CheckResultsRepository.php
+++ Classes/Domain/Repository/CheckResultsRepository.php
@@ -2,21 +2,23 @@
 
 namespace Homeinfo\SysMon2\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 
 class CheckResultsRepository extends Repository
 {
     public function findBySystem(int $system): QueryResultInterface {
-        $query = $this->getStorageIndependentQuery();
-        return $query
-            ->matching($query->equals('system', $system))
-            ->execute();
+        $query = $this->getStorageIndependentQuery()->matching($query->equals('system', $system));
+        DebuggerUtility::var_dump($query, "Final query: ");
+        return $query->execute();
     }
 
     private function getStorageIndependentQuery() {
         $query = $this->createQuery();
+        DebuggerUtility::var_dump($query, "Raw query: ");
         $query->getQuerySettings()->setRespectStoragePage(FALSE);
+        DebuggerUtility::var_dump($query, "Query without storage: ");
         return $query;
     }
 }
