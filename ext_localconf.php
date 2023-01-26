4f1a7c7775d1a1c9282c1c0abc3eae41bb740b38
Richard Neumann <r.neumann@homeinfo.de>
2023-01-26 13:03:54 +0100

    Rename view

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'SysMon2',
-    'SysMon2',
+    'LastMonth',
     [
         \Homeinfo\SysMon2\Controller\UnauthenticatedAccess::class => 'listLastMonth',
     ],
