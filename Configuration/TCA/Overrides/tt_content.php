758dd069b8018adcb2228022d904441893bc29e7
Richard Neumann <r.neumann@homeinfo.de>
2023-02-07 10:55:24 +0100

    Rename plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -6,7 +6,7 @@ $extensionKey = 'sysmon2';
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'sysmon2',
     'SysMon2',
-    'SysMon2',
+    'LastMonth',
     'EXT:'.$extensionKey.'/Resources/Public/Icons/Extension.svg'
 );
 ?>
