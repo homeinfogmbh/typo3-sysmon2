3bf9fe2b0329ebab00918a2439cbbc60dc3c5dcf
Richard Neumann <r.neumann@homeinfo.de>
2023-02-07 14:35:26 +0100

    Add tt_content

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+defined('TYPO3') or die();
+
+ExtensionUtility::registerPlugin(
+    'SysMon2',
+    'LastMonth',
+    "View of last month's system checks"
+);
