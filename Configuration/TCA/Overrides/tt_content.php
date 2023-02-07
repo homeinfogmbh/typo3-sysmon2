096da23a293cdb70e4bf1e71853f4cb5c200ebaa
Richard Neumann <r.neumann@homeinfo.de>
2023-02-07 14:36:40 +0100

    Use lower case IDs

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -6,7 +6,7 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 defined('TYPO3') or die();
 
 ExtensionUtility::registerPlugin(
-    'SysMon2',
+    'sysmon2',
     'LastMonth',
     "View of last month's system checks"
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'SysMon2',
+    'sysmon2',
     'LastMonth',
     [
         \Homeinfo\SysMon2\Controller\UnauthenticatedAccess::class => 'listLastMonth',
