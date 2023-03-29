f7ffb3407509334185a8595d5aa975d8d4688089
Richard Neumann <r.neumann@homeinfo.de>
2023-03-29 10:01:54 +0200

    Update threshold values

--- Classes/Domain/Model/CheckResults.php
+++ Classes/Domain/Model/CheckResults.php
@@ -4,8 +4,8 @@ namespace Homeinfo\SysMon2\Domain\Model;
 
 use DateTime;
 
-const MIN_DOWNLOAD = 2048;  // Mbit/s
-const MIN_UPLOAD = 10;      // Mbit/s
+const MIN_DOWNLOAD = 1953.125;  // Mebibit/s
+const MIN_UPLOAD = 488.28125;   // Mebibit/s
 
 final class CheckResults
 {
